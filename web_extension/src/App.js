250af21505fcd9fcb7bd09c6449e08ca5c0ee09b
Jonathan Segal <jonathannsegal@gmail.com>
2020-07-05 23:59:21 -0700

    🗺️ Google Places API for image.

--- web_extension/src/App.js
+++ web_extension/src/App.js
@@ -15,7 +15,8 @@ class App extends React.Component {
       lng: 0,
       lat: 0,
       zoom: 13,
-      data: []
+      data: [],
+      image: ''
     };
 
     this.findCity = this.findCity.bind(this);
@@ -32,7 +33,7 @@ class App extends React.Component {
 
     map = new mapboxgl.Map({
       container: this.mapContainer,
-      style: 'mapbox://styles/mapbox/satellite-v9',
+      style: 'mapbox://styles/mapbox/satellite-streets-v11',
       center: [this.state.lng, this.state.lat],
       zoom: this.state.zoom
     });
@@ -66,11 +67,13 @@ class App extends React.Component {
                 this.setState({
                   data: result.data[0]
                 });
+                this.fetchCityData(`${result.data[0]?.city}, ${result.data[0]?.country}`);
                 map.jumpTo({
                   center: [
                     result.data[0].longitude.toFixed(4),
                     result.data[0].latitude.toFixed(4)
-                  ]
+                  ],
+                  zoom: 13
                 });
               },
               (error) => {
@@ -87,7 +90,24 @@ class App extends React.Component {
             error
           });
         }
-      )
+      );
+  }
+
+
+  fetchCityData(searchQuery) {
+    fetch(`http://allow-any-origin.appspot.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${searchQuery}&inputtype=textquery&fields=photos&key=${process.env.REACT_APP_GOOGLE_PLACES_API_KEY}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          fetch(`http://allow-any-origin.appspot.com/https://maps.googleapis.com/maps/api/place/photo?maxheight=800&photoreference=${result?.candidates[0]?.photos[0]?.photo_reference}&key=${process.env.REACT_APP_GOOGLE_PLACES_API_KEY}`)
+            .then(res => res.blob())
+            .then(
+              (image) => {
+                this.setState({
+                  image: URL.createObjectURL(image)
+                });
+              });
+        });
   }
 
   handlePlay() {
@@ -97,10 +117,14 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <div id="button" ref={ref => this.animBox = ref} onClick={() =>{
+        <div id="button" ref={ref => this.animBox = ref} onClick={() => {
           this.handlePlay();
           this.findCity();
         }} />
+        <div className="sidebar">
+          <img width="100%" src={this.state.image} alt="city" />
+          <p>{this.state.data?.city}</p>
+        </div>
         <div>
           <div ref={el => this.mapContainer = el} className='mapContainer' />
         </div>
