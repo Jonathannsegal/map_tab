7a049b49d90bab2433410c7ceb2f25de0bf69060
Jonathan Segal <jonathannsegal@gmail.com>
2020-07-06 21:02:04 -0700

    🎉 Website 0.1.0!

--- website/pages/index.js
+++ website/pages/index.js
@@ -1,192 +1,153 @@
 import Head from 'next/head'
+import { useState, useEffect } from 'react';
+import ReactMapGL from 'react-map-gl';
+import Lottie from 'react-lottie'
+import * as dice from '../public/dice.json'
+
+const diceOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: dice.default,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice'
+  }
+};
 
 export default function Home() {
+  const [viewport, setViewport] = useState({
+    width: '100vw',
+    height: '100vh',
+    latitude: 0,
+    longitude: 0,
+    zoom: 14,
+  });
+  const [info, setInfo] = useState({
+    image: ''
+  });
+  const [animation, setAnimation] = useState({
+    isPaused: false
+  });
+
+  useEffect(() => {
+    findCity();
+  }, []);
+
+  function findCity() {
+    fetch("http://geodb-free-service.wirefreethought.com/v1/geo/cities?hateoasMode=off")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          fetch(`http://geodb-free-service.wirefreethought.com/v1/geo/cities?limit=1&offset=${Math.floor(Math.random() * result.metadata.totalCount)}&hateoasMode=off`)
+            .then(res => res.json())
+            .then(
+              (result) => {
+                setInfo(x => ({ ...x, data: result }));
+                fetchCityData(`${result.data[0]?.city}, ${result.data[0]?.country}`);
+                setViewport(x => ({
+                  ...x,
+                  longitude: parseInt(result.data[0]?.longitude.toFixed(4)),
+                  latitude: parseInt(result.data[0]?.latitude.toFixed(4))
+                }));
+              })
+        });
+  };
+
+  function fetchCityData(searchQuery) {
+    fetch(`http://allow-any-origin.appspot.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${searchQuery}&inputtype=textquery&fields=photos&key=${process.env.REACT_APP_GOOGLE_PLACES_API_KEY}`)
+    .then(res => res.json())
+    .then(
+      (result) => {
+        fetch(`http://allow-any-origin.appspot.com/https://maps.googleapis.com/maps/api/place/photo?maxheight=800&photoreference=${result?.candidates[0]?.photos[0]?.photo_reference}&key=${process.env.REACT_APP_GOOGLE_PLACES_API_KEY}`)
+        .then(res => res.blob())
+        .then(
+          (image) => {
+            setInfo(x => ({ ...x, image: URL.createObjectURL(image) }))
+          });
+      });
+  }
+
+  function handleClick() {
+    setAnimation(x => ({ ...x, isPaused: false }));
+    findCity();
+  };
+
   return (
-    <div className="container">
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>Map Tab</title>
         <link rel="icon" href="/favicon.ico" />
+        <link href='https://api.mapbox.com/mapbox-gl-js/v1.11.0/mapbox-gl.css' rel='stylesheet' />
       </Head>
-
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div id="button" onClick={() => handleClick()}>
+          <Lottie
+            {...animation}
+            options={diceOptions}
+            isClickToPauseDisabled={true}
+            eventListeners={
+              [
+                {
+                  eventName: 'enterFrame',
+                  callback: obj => {
+                    if (obj.currentTime > (obj.totalTime - 1)) {
+                      setAnimation(x => ({ ...x, isPaused: true }));
+                    }
+                  },
+                },
+              ]
+            }
+          />
         </div>
+        <div className="sidebar">
+          <img src={info.image} alt="city" />
+          <h1>{info.data?.data[0]?.city}</h1>
+          <p>{info.data?.data[0]?.country}</p>
+          <p>{`${info.data?.data[0]?.latitude.toFixed(4)}, ${info.data?.data[0]?.longitude.toFixed(4)}`}</p>
+        </div>
+        <ReactMapGL
+          {...viewport}
+          mapStyle="mapbox://styles/mapbox/satellite-streets-v11"
+          mapboxApiAccessToken={process.env.REACT_APP_ACCESS_TOKEN}
+          onViewportChange={nextViewport => setViewport(nextViewport)}
+        />
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
       <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+        #button {
+          cursor: pointer;
+          display: inline-block;
+          position: absolute;
+          right: 0;
+          top: 0;
+          width: 5em;
+          height: 5em;
+          margin-top: 5px;
+          margin-right: 15px;
+          z-index: 1 !important;
+          font-weight: bold;
         }
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
+        .sidebar {
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
           text-align: center;
+          border-bottom: 1px solid #E8EAED;
+          box-shadow: 0 2px 4px rgba(0,0,0,0.3);
+          border-radius: 8px;
+          margin: 2em 0 2em 2em;
+          display: inline-block;
+          position: absolute;
+          left: 0;
+          z-index: 1 !important;
+          background-color: white;
+          width: 300px;
         }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
+        
+        .sidebar img {
+          border-radius: 8px 8px 0 0;
+          width: 100%;
+          height: 200px;
+          object-fit: cover;
         }
       `}</style>
 
@@ -204,6 +165,6 @@ export default function Home() {
           box-sizing: border-box;
         }
       `}</style>
-    </div>
+    </>
   )
 }
